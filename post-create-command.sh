b6c5b30f6d35a1fe617b55d3ab8dcad054930541
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-04-22 12:48:10 +0000

    precommit

--- post-create-command.sh
+++ post-create-command.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pre-commit install
-
 chmod 600 /root/.ssh/id_rsa
 
 ssh-agent bash -c 'ssh-add /root/.ssh/id_rsa'
+
+pre-commit install
 
 if [ ! -d "debugvisualizer" ]; then
     git clone git@github.com:PARKCHEOLHEE-lab/debugvisualizer.git
