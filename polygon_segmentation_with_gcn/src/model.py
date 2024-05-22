1299cbff89ed0bb78be1d5be8a3e71e8047c2126
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-05-22 10:42:32 +0000

    model.py in progress

--- polygon_segmentation_with_gcn/src/model.py
+++ polygon_segmentation_with_gcn/src/model.py
@@ -8,92 +8,166 @@ if os.path.abspath(os.path.join(__file__, "../../../")) not in sys.path:
     sys.path.append(os.path.abspath(os.path.join(__file__, "../../../")))
 
 import torch
+import torch.nn as nn
 
-from typing import Tuple, List
-from torch.utils.data import ConcatDataset, random_split
-from torch_geometric.data import DataLoader, Dataset, Data
+from tqdm import tqdm
+from torch_geometric.data import Batch
+from torch_geometric.nn import GCNConv, Sequential
+from torch_geometric.utils import negative_sampling
+from polygon_segmentation_with_gcn.src.commonutils import runtime_calculator
 from polygon_segmentation_with_gcn.src.config import Configuration
+from polygon_segmentation_with_gcn.src.dataset import PolygonGraphDataset
 
 
-class RegularPolygonDataset(Dataset):
-    def __init__(self, data_path: str = Configuration.MERGED_SAVE_PATH):
-        self.regular_polygons: List[Data]
-        self.regular_polygons = []
-        for file in os.listdir(data_path):
-            if Configuration.LANDS_DATA_REGULAR_PT in file:
-                self.regular_polygons.extend(torch.load(os.path.join(data_path, file)))
-
-    def __len__(self):
-        return len(self.regular_polygons)
-
-    def __getitem__(self, index):
-        return self.regular_polygons[index]
-
-
-class IrregularPolygonDataset(Dataset):
-    def __init__(self, data_path: str = Configuration.MERGED_SAVE_PATH):
-        self.irregular_polygons: List[Data]
-        self.irregular_polygons = []
-        for file in os.listdir(data_path):
-            if Configuration.LANDS_DATA_IRREGULAR_PT in file:
-                self.irregular_polygons.extend(torch.load(os.path.join(data_path, file)))
-
-    def __len__(self):
-        return len(self.irregular_polygons)
-
-    def __getitem__(self, index):
-        return self.irregular_polygons[index]
-
-
-class PolygonGraphDataset(Dataset):
-    def __init__(self, regular_polygons: RegularPolygonDataset, irregular_polygons: IrregularPolygonDataset):
-        self.regular_polygons = regular_polygons
-        self.irregular_polygons = irregular_polygons
-
-        self.train_dataloader, self.validation_dataloder, self.test_dataloader = self._get_dataloaders(
-            self.regular_polygons, self.irregular_polygons
+class PolygonSegmenterGCN(nn.Module):
+    def __init__(self, in_channels: int, hidden_channels: int, out_channels: int):
+        super().__init__()
+        self.main = Sequential(
+            input_args="x, edge_index, edge_weight",
+            modules=[
+                (GCNConv(in_channels, hidden_channels), "x, edge_index, edge_weight -> x"),
+                nn.ReLU(inplace=True),
+                nn.Dropout(Configuration.DROPOUT_RATE),
+                (GCNConv(hidden_channels, hidden_channels), "x, edge_index, edge_weight -> x"),
+                nn.ReLU(inplace=True),
+                nn.Dropout(Configuration.DROPOUT_RATE),
+                (GCNConv(hidden_channels, hidden_channels), "x, edge_index, edge_weight -> x"),
+                nn.ReLU(inplace=True),
+                nn.Dropout(Configuration.DROPOUT_RATE),
+                (GCNConv(hidden_channels, hidden_channels), "x, edge_index, edge_weight -> x"),
+                nn.ReLU(inplace=True),
+                nn.Dropout(Configuration.DROPOUT_RATE),
+                (GCNConv(hidden_channels, out_channels), "x, edge_index, edge_weight -> x"),
+            ],
         )
 
-    def _get_dataloaders(
-        self, regular_polygons: RegularPolygonDataset, irregular_polygons: IrregularPolygonDataset
-    ) -> Tuple[DataLoader]:
-        regular_train, regular_validation, regular_test = random_split(regular_polygons, Configuration.SPLIT_RATIOS)
-        irregular_train, irregular_validation, irregular_test = random_split(
-            irregular_polygons, Configuration.SPLIT_RATIOS
+        self.to(Configuration.DEVICE)
+
+    def encode(self, data: Batch) -> torch.Tensor:
+        return self.main(data.x, data.edge_index, edge_weight=data.edge_weight)
+
+    def decode(self, encoded: torch.Tensor, edge_label_index: torch.Tensor) -> torch.Tensor:
+        return (encoded[edge_label_index[0]] * encoded[edge_label_index[1]]).sum(dim=1)
+
+    def forward(self, data: Batch) -> torch.Tensor:
+        # Encode the features of polygon graphs
+        encoded = self.encode(data)
+
+        # Sample negative edges
+        negative_edge_index = negative_sampling(
+            edge_index=data.edge_index,
+            num_nodes=data.num_nodes,
+            num_neg_samples=data.edge_label_index.shape[1],
+            method="sparse",
         )
 
-        splitted_regular = torch.tensor([len(regular_train), len(regular_validation), len(regular_test)])
-        expected_regular = torch.tensor([len(regular_polygons) * ratio for ratio in Configuration.SPLIT_RATIOS])
-        assert torch.all(splitted_regular == expected_regular)
+        # Decode the encoded features of the nodes to predict whether the edges are connected
+        decoded = self.decode(encoded, edge_label_index=torch.hstack([data.edge_label_index, negative_edge_index]))
 
-        splitted_irregular = torch.tensor([len(irregular_train), len(irregular_validation), len(irregular_test)])
-        expected_irregular = torch.tensor([len(irregular_polygons) * ratio for ratio in Configuration.SPLIT_RATIOS])
-        assert torch.all(splitted_irregular == expected_irregular)
+        return decoded
 
-        train_dataset = ConcatDataset([regular_train, irregular_train])
-        validation_dataset = ConcatDataset([regular_validation, irregular_validation])
-        test_dataset = ConcatDataset([regular_test, irregular_test])
 
-        train_dataloader = DataLoader(
-            train_dataset,
-            batch_size=Configuration.BATCH_SIZE,
-            shuffle=True,
-        )
+class PolygonSegmenterTrainer:
+    def __init__(
+        self,
+        dataset: PolygonGraphDataset,
+        model: nn.Module,
+        loss_function: nn.modules.loss._Loss,
+        optimizer: torch.optim.Optimizer,
+    ):
+        self.dataset = dataset
+        self.model = model
+        self.loss_function = loss_function
+        self.optimizer = optimizer
 
-        validation_dataloader = DataLoader(
-            validation_dataset,
-            batch_size=Configuration.BATCH_SIZE,
-            shuffle=True,
-        )
+    @runtime_calculator
+    def _train_each_epoch(
+        self,
+        dataset: PolygonGraphDataset,
+        model: nn.Module,
+        loss_function: nn.modules.loss._Loss,
+        optimizer: torch.optim.Optimizer,
+        epoch: int,
+    ) -> float:
+        """_summary_
 
-        test_dataloader = DataLoader(
-            test_dataset,
-            batch_size=Configuration.BATCH_SIZE,
-            shuffle=True,
-        )
+        Args:
+            dataset (PolygonGraphDataset): _description_
+            model (nn.Module): _description_
+            loss_function (nn.modules.loss._Loss): _description_
+            optimizer (torch.optim.Optimizer): _description_
+            epoch (int): _description_
 
-        return train_dataloader, validation_dataloader, test_dataloader
+        Returns:
+            float: _description_
+        """
+
+        train_losses = []
+        for data_to_train in tqdm(dataset.train_dataloader, desc=f"Training... epoch: {epoch}/{Configuration.EPOCH}"):
+            decoded = model(data_to_train)
+
+            labels = torch.hstack([data_to_train.edge_label, torch.zeros_like(data_to_train.edge_label)])
+            loss = loss_function(decoded, labels)
+
+            train_losses.append(loss.item())
+
+            model.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        return sum(train_losses) / len(train_losses)
+
+    @torch.no_grad()
+    def _evaluate_each_epoch(
+        self, dataset: PolygonGraphDataset, model: nn.Module, loss_function: nn.modules.loss._Loss, epoch: int
+    ):
+        model.eval()
+
+        validation_losses = []
+        for data_to_validate in tqdm(
+            dataset.validation_dataloder, desc=f"Evaluating... epoch: {epoch}/{Configuration.EPOCH}"
+        ):
+            decoded = model(data_to_validate)
+
+            labels = torch.hstack([data_to_validate.edge_label, torch.zeros_like(data_to_validate.edge_label)])
+            loss = loss_function(decoded, labels)
+
+            validation_losses.append(loss.item())
+
+        model.train()
+
+        return sum(validation_losses) / len(validation_losses)
+
+    def train(self):
+        for epoch in range(1, Configuration.EPOCH + 1):
+            avg_train_loss = self._train_each_epoch(self.dataset, self.model, self.loss_function, self.optimizer, epoch)
+            avg_validation_loss = self._evaluate_each_epoch(self.dataset, self.model, self.loss_function, epoch)
+
+            print(avg_train_loss)
+            print(avg_validation_loss)
+
+            if epoch == 2:
+                break
 
 
 if __name__ == "__main__":
-    PolygonGraphDataset(regular_polygons=RegularPolygonDataset(), irregular_polygons=IrregularPolygonDataset())
+    # from debugvisualizer.debugvisualizer import Plotter
+    # from shapely import geometry
+
+    Configuration.set_seed()
+
+    dataset = PolygonGraphDataset()
+    model = PolygonSegmenterGCN(
+        in_channels=dataset.regular_polygons[0].x.shape[1],
+        hidden_channels=Configuration.HIDDEN_CHANNELS,
+        out_channels=Configuration.OUT_CHANNELS,
+    )
+
+    loss_function = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=Configuration.LEARNING_RATE)
+
+    polygon_segmenter_trainer = PolygonSegmenterTrainer(
+        dataset=dataset, model=model, loss_function=loss_function, optimizer=optimizer
+    )
+
+    polygon_segmenter_trainer.train()
