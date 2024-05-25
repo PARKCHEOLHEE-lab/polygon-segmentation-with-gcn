01a98f23d3cf6491dd367ffc2c8311bda41f3c77
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-05-25 05:38:13 +0000

    training

--- polygon_segmentation_with_gcn/src/model.py
+++ polygon_segmentation_with_gcn/src/model.py
@@ -10,7 +10,10 @@ if os.path.abspath(os.path.join(__file__, "../../../")) not in sys.path:
 import torch
 import torch.nn as nn
 import datetime
+import numpy as np
+import matplotlib.pyplot as plt
 
+from PIL import Image
 from shapely import geometry
 from typing import List
 from tqdm import tqdm
@@ -56,10 +59,33 @@ class PolygonSegmenterGCN(nn.Module):
         self.to(Configuration.DEVICE)
 
     def encode(self, data: Batch) -> torch.Tensor:
-        return self.main(data.x, data.edge_index, edge_weight=data.edge_weight)
+        """_summary_
+
+        Args:
+            data (Batch): _description_
+
+        Returns:
+            torch.Tensor: _description_
+        """
+
+        encoded = self.main(data.x, data.edge_index, edge_weight=data.edge_weight)
+
+        return encoded
 
     def decode(self, encoded: torch.Tensor, edge_label_index: torch.Tensor) -> torch.Tensor:
-        return (encoded[edge_label_index[0]] * encoded[edge_label_index[1]]).sum(dim=1)
+        """_summary_
+
+        Args:
+            encoded (torch.Tensor): _description_
+            edge_label_index (torch.Tensor): _description_
+
+        Returns:
+            torch.Tensor: _description_
+        """
+
+        decoded = (encoded[edge_label_index[0]] * encoded[edge_label_index[1]]).sum(dim=1)
+
+        return decoded.sigmoid()
 
     @torch.no_grad()
     def infer(self, data_to_infer: Batch) -> List[torch.Tensor]:
@@ -76,7 +102,7 @@ class PolygonSegmenterGCN(nn.Module):
             mask_to_ignore[each_data.edge_index[0], each_data.edge_index[1]] = False
             mask_to_ignore[each_data.edge_index[1], each_data.edge_index[0]] = False
 
-            connection_probability = encoded @ encoded.t()
+            connection_probability = (encoded @ encoded.t()).sigmoid()
             connection_probability *= mask_to_ignore.long()
 
             infered = (connection_probability > Configuration.CONNECTIVITY_THRESHOLD).nonzero().t()
@@ -137,11 +163,8 @@ class PolygonSegmenterTrainer:
             self.states = torch.load(self.states_path)
             print(f"Load pre-trained states from {self.states_path} \n")
 
-        self.images_dir = os.path.join(self.log_dir, "images")
-        os.makedirs(self.images_dir, exist_ok=True)
-
     def _set_loss_function(self):
-        self.loss_function = nn.BCEWithLogitsLoss()
+        self.loss_function = nn.BCELoss()
 
     def _set_optimizer(self):
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=Configuration.LEARNING_RATE)
@@ -209,7 +232,9 @@ class PolygonSegmenterTrainer:
 
         return sum(validation_losses) / len(validation_losses)
 
-    def evaluate(self, dataset: PolygonGraphDataset, model: nn.Module) -> None:
+    def evaluate_qualitatively(
+        self, dataset: PolygonGraphDataset, model: nn.Module, epoch: int, viz_count: int = 10
+    ) -> None:
         """_summary_
 
         Args:
@@ -217,39 +242,156 @@ class PolygonSegmenterTrainer:
             model (nn.Module): _description_
         """
 
-        c = 4
-
-        train_indices_to_viz = torch.randperm(len(dataset.train_dataloader))[:c]
+        train_indices_to_viz = torch.randperm(len(dataset.train_dataloader))[:viz_count]
         train_subset = Subset(dataset.train_dataloader.dataset, train_indices_to_viz)
-        train_sampled = DataLoader(train_subset, batch_size=c)
+        train_sampled = DataLoader(train_subset, batch_size=viz_count)
 
-        validation_indices_to_viz = torch.randperm(len(dataset.validation_dataloder))[:c]
+        validation_indices_to_viz = torch.randperm(len(dataset.validation_dataloder))[:viz_count]
         validation_subset = Subset(dataset.validation_dataloder.dataset, validation_indices_to_viz)
-        validation_sampled = DataLoader(validation_subset, batch_size=c)
+        validation_sampled = DataLoader(validation_subset, batch_size=viz_count)
 
         train_batch = [data_to_infer for data_to_infer in train_sampled][0]
         validation_batch = [data_to_infer for data_to_infer in validation_sampled][0]
 
         train_segmentation_indices = model.infer(train_batch)
-        for si in range(len(train_segmentation_indices)):
-            each_data = train_batch[si]
-            each_segmentation_indices = train_segmentation_indices[si]
+        validation_segmentation_indices = model.infer(validation_batch)
 
-            polygon = geometry.Polygon(each_data.x[:, :2].detach().cpu().numpy())
-            predicted_edges = DataCreatorHelper.connect_polygon_segments_by_indices(polygon, each_segmentation_indices)
-            label_edges = DataCreatorHelper.connect_polygon_segments_by_indices(
-                polygon, each_data.edge_label_index_only
+        dpi = 100
+        figsize = (5, 5)
+
+        figures = []
+
+        for tsi in range(len(train_segmentation_indices)):
+            each_train_data = train_batch[tsi]
+            each_train_segmentation_indices = train_segmentation_indices[tsi]
+
+            train_polygon = geometry.Polygon(each_train_data.x[:, :2].detach().cpu().numpy())
+            train_predicted_edges = DataCreatorHelper.connect_polygon_segments_by_indices(
+                train_polygon, each_train_segmentation_indices
+            )
+            train_label_edges = DataCreatorHelper.connect_polygon_segments_by_indices(
+                train_polygon, each_train_data.edge_label_index_only
             )
 
-            print(predicted_edges)
-            print(label_edges)
+            figure = plt.figure(figsize=figsize, dpi=dpi)
+            ax = figure.add_subplot(1, 1, 1)
+            ax.axis("equal")
 
-            # self.summary_writer.add_figure(
-            #     f"train_segmentation_{si}", Plotter.plot_polygon_segmentation(polygon, predicted_edges, label_edges)
-            # )
+            added_predicted_label = False
+            for train_predicted_edge in train_predicted_edges:
+                if added_predicted_label:
+                    ax.plot(*train_predicted_edge.coords.xy, color="green", linewidth=1.0, alpha=0.2)
+                else:
+                    added_predicted_label = True
+                    ax.plot(
+                        *train_predicted_edge.coords.xy,
+                        color="green",
+                        linewidth=1.0,
+                        alpha=0.2,
+                        label="train predicted",
+                    )
 
-        validation_segmentation_indices = model.infer(validation_batch)
-        print(validation_segmentation_indices)
+            ax.plot(*train_polygon.exterior.coords.xy, color="black", linewidth=0.6, label="train polygon")
+            ax.fill(*train_polygon.exterior.coords.xy, alpha=0.1, color="black")
+
+            added_ground_truth_label = False
+            for train_label_edge in train_label_edges:
+                if added_ground_truth_label:
+                    ax.plot(*train_label_edge.coords.xy, color="blue", linewidth=1.0)
+                else:
+                    added_ground_truth_label = True
+                    ax.plot(*train_label_edge.coords.xy, color="blue", linewidth=1.0, label="train ground truth")
+
+            x, y = train_polygon.exterior.coords.xy
+            ax.scatter(x, y, color="red", s=7, label="vertices")
+            ax.grid(True, color="lightgray")
+
+            plt.axis([-2.0, 2.0, -2.0, 2.0])
+            plt.legend()
+
+            figures.append(figure)
+
+            plt.close(figure)
+
+        for vsi in range(len(validation_segmentation_indices)):
+            each_validation_data = validation_batch[vsi]
+            each_validation_segmentation_indices = validation_segmentation_indices[vsi]
+
+            validation_polygon = geometry.Polygon(each_validation_data.x[:, :2].detach().cpu().numpy())
+            validation_predicted_edges = DataCreatorHelper.connect_polygon_segments_by_indices(
+                validation_polygon, each_validation_segmentation_indices
+            )
+            validation_label_edges = DataCreatorHelper.connect_polygon_segments_by_indices(
+                validation_polygon, each_validation_data.edge_label_index_only
+            )
+
+            figure = plt.figure(figsize=figsize, dpi=dpi)
+            ax = figure.add_subplot(1, 1, 1)
+            ax.axis("equal")
+
+            added_predicted_label = False
+            for validation_predicted_edge in validation_predicted_edges:
+                if added_predicted_label:
+                    ax.plot(*validation_predicted_edge.coords.xy, color="green", linewidth=1.0, alpha=0.2)
+                else:
+                    added_predicted_label = True
+                    ax.plot(
+                        *validation_predicted_edge.coords.xy,
+                        color="green",
+                        linewidth=1.0,
+                        alpha=0.2,
+                        label="validation predicted",
+                    )
+
+            ax.plot(*validation_polygon.exterior.coords.xy, color="black", linewidth=0.6, label="validation polygon")
+            ax.fill(*validation_polygon.exterior.coords.xy, alpha=0.1, color="black")
+
+            added_ground_truth_label = False
+            for validation_label_edge in validation_label_edges:
+                if added_ground_truth_label:
+                    ax.plot(*validation_label_edge.coords.xy, color="blue", linewidth=1.0)
+                else:
+                    added_ground_truth_label = True
+                    ax.plot(
+                        *validation_label_edge.coords.xy, color="blue", linewidth=1.0, label="validation ground truth"
+                    )
+
+            x, y = validation_polygon.exterior.coords.xy
+            ax.scatter(x, y, color="red", s=7, label="vertices")
+            ax.grid(True, color="lightgray")
+
+            plt.axis([-2.0, 2.0, -2.0, 2.0])
+            plt.legend()
+
+            figures.append(figure)
+
+            plt.close(figure)
+
+        col_num = 5
+        row_num = int(np.ceil((viz_count * 2) / col_num))
+        img_size = figsize[0] * dpi
+        merged_image = Image.new("RGB", (col_num * img_size, row_num * img_size), "white")
+
+        current_cols = 0
+        output_height = 0
+        output_width = 0
+
+        for figure in figures:
+            image = DataCreatorHelper.fig_to_img(figure)
+
+            merged_image.paste(image, (output_width, output_height))
+
+            current_cols += 1
+            if current_cols >= col_num:
+                output_width = 0
+                output_height += img_size
+                current_cols = 0
+            else:
+                output_width += img_size
+
+        self.summary_writer.add_image(
+            f"train_segmentation_{epoch}", np.array(merged_image), dataformats="HWC", step=epoch
+        )
 
     def train(self) -> None:
         """_summary_"""
@@ -291,7 +433,7 @@ class PolygonSegmenterTrainer:
             self.summary_writer.add_scalar("train_loss", avg_train_loss, epoch)
             self.summary_writer.add_scalar("validation_loss", avg_validation_loss, epoch)
 
-            self.evaluate(self.dataset, self.model)
+            self.evaluate_qualitatively(self.dataset, self.model, epoch)
 
 
 if __name__ == "__main__":
